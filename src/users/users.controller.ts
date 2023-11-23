202a5a444ad2dbb6dc893563979a8fc3fd842ed0
hungtdreact <hungtd@reactplus.com>
2023-11-23 16:16:18 +0700

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,24 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './users.model';
 import * as bcrypt from 'bcrypt';
+import {CreateUserDto} from "./users.type";
 
 @Controller('api/v1')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
     @Post('/signup')
-    async createUser(
-        @Body('password') password: string,
-        @Body('username') username: string,
-    ): Promise<User> {
+    async createUser(@Body() createUserDto: CreateUserDto) {
+        const {username,password} = createUserDto
+        if (username.length < 4 || password.length < 4) return {
+            message: 'Username and password must be at least 4 characters long'
+        };
         const saltOrRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltOrRounds);
-        const result = await this.usersService.createUser(
+        const newUser = await this.usersService.createUser(
             username,
             hashedPassword,
         );
-        return result;
+        return newUser;
     }
 }
---
+++ src/users/users.type.ts
@@ -0,0 +1,4 @@
+export class CreateUserDto {
+    readonly password: string;
+    readonly username: string;
+}
