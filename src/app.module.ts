619774b97a51c82ae0066b2191fc0b426cb81573
hungtdreact <hungtd@reactplus.com>
2023-11-23 15:34:26 +0700

    add signin & signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EventModule } from './event/event.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://hungtd172600:PVCiboKo1sjbOrXF@cluster0.gsyvx5t.mongodb.net/?retryWrites=true&w=majority', {
       autoCreate: true
     }),
-    EventModule],
+    EventModule,
+    UsersModule,
+    AuthModule],
   controllers: [],
   providers: []
 })
